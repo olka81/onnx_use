eaff7bb1b02d9cff14ae15bf07e358e20f57f47b
Olga Brattseva <brattseva81@gmail.com>
2025-07-27 19:39:21 +0300

    +model, +debug

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,18 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Compiled against ORT API version: " << ORT_API_VERSION << std::endl;
 
-    Ort::Env env{ORT_LOGGING_LEVEL_WARNING, "test"};
-    std::cout << "ONNX Runtime initialized." << std::endl;
+    std::wcout << L"Compiled against ORT API version: " << ORT_API_VERSION << std::endl;
 
+    Ort::Env env{ORT_LOGGING_LEVEL_WARNING, "demo"};
+
+    Ort::SessionOptions session_options;
+    session_options.SetIntraOpNumThreads(1);
+    session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
+
+    std::wstring model_path = L"model-8.onnx";
+    Ort::Session session(env, model_path.c_str(), session_options);
+
+    std::wcout << L"Model loaded successfully." << std::endl;
     return 0;
 }

e4d053f93fdac5bafa0d3beb656971956b611311
Olga Brattseva <brattseva81@gmail.com>
2025-07-28 18:14:27 +0300

    +inputs

--- src/main.cpp
+++ src/main.cpp
@@ -20,14 +20,33 @@ int main() {
 
     std::wstring model_path = L"" MODEL_PATH;
     std::wcout << L"Model path: " << model_path << std::endl;
+    std::unique_ptr<Ort::Session> session;
 
     try {
-        Ort::Session session(env, model_path.c_str(), session_options);
+        session = std::make_unique<Ort::Session>(env, model_path.c_str(), session_options);
         std::wcout << L"Model loaded successfully." << std::endl;
     } catch (const Ort::Exception& e) {
         std::wcerr << L"ORT Exception: " << e.what() << std::endl;
         return 1;
     }
+    Ort::AllocatorWithDefaultOptions allocator;
 
+    // Входы
+    size_t num_inputs = session->GetInputCount();
+    std::wcout << L"Number of inputs: " << num_inputs << std::endl;
+
+    for (size_t i = 0; i < num_inputs; ++i) {
+        Ort::AllocatedStringPtr input_name = session->GetInputNameAllocated(i, allocator);
+        std::wcout << L"Input " << i << L" name: " << input_name.get() << std::endl;
+
+        Ort::TypeInfo type_info = session->GetInputTypeInfo(i);
+        auto tensor_info = type_info.GetTensorTypeAndShapeInfo();
+        std::vector<int64_t> shape = tensor_info.GetShape();
+
+        std::wcout << L"  Shape: [ ";
+        for (auto dim : shape) std::wcout << dim << L" ";
+        std::wcout << L"]" << std::endl;
+    }
+    
     return 0;
 }
